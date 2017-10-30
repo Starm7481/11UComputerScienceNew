6e01f0fa8457acb3df1f066e66a1f93833ba64bb
Starm7481 <Starm7481@HRH-IC0027326.wrdsb.ca>
2017-10-30 13:07:24 -0400

    UbbiDubbi trans working

--- src/UbbiAssignment/UbbiDubbitrans.java
+++ src/UbbiAssignment/UbbiDubbitrans.java
@@ -28,6 +28,9 @@ public class UbbiDubbitrans {
          * single vowel and double vowel 
          * 
          */
+        int x = 0;
+        
+        while(x < 1){
         
          System.out.print("Please enter your word: ");
 
@@ -35,57 +38,49 @@ public class UbbiDubbitrans {
 
             String word = words.nextLine();
             
-            int x = 0;
+            String ex = "EXIT";
             
-            while(x < 1){
+            if(word.equals(ex)){
                 
+                System.out.print("goodbye");
                 
-                for(int i = 0; i < word.length(); i++){
-                    
-                if(word.charAt(i) == 'a' || word.charAt(i) == 'e' || word.charAt(i) == 'i' || word.charAt(i) == 'o' || word.charAt(i) == 'u' ){
-                    
-                    String front = word.substring(0, i);
-                    
-                    String ending = word.substring(i);
-                    // asseble translation 
-                    
-                    String translated =  front + "ub" + ending;
-                    
-                     String first = front + "ub";
-                    
-                     for(int s = 0; s < ending.length(); s++){
-                    // loop to find a second variable 
-                     if(ending.charAt(s) == 'a' || ending.charAt(s) == 'e' || ending.charAt(s) == 's' || ending.charAt(s) == 'o' || ending.charAt(s) == 'u' ){
-                         
-                         
-                    String sfront = ending.substring(0, i);
-                    
-                    String sending = ending.substring(i);
-                    // asseble translation 
-                    
-                    String ssending =  first + sfront + "ub" + sending;
-                    
-                    System.out.println(word + " in UbbiDubbi is " + ssending);
-                    
-                    // assemble whole word 
-                    
-                     }
-                    
-                     
-                     }
-                    
-                    
-                    
-                    
-                    
-                    x = x+1;
-                    
-                    break;   
-                    
-                    
-                    
-                }
+               break;
+                
             }
+            
+            String translate = "";
+            
+            Boolean foundvowel = false; 
+            
+            for(int i = 0; i < word.length(); i++){
+                
+                // look for a vowel at spot i 
+                if((word.charAt(i) == 'a' || word.charAt(i) == 'e' || word.charAt(i) == 'i' || word.charAt(i) == 'o' || word.charAt(i) == 'u')  && foundvowel == false){
+                    
+                  translate = translate + "ub";
+                  
+                  translate = translate + word.charAt(i);
+                  
+                  foundvowel = true; 
+                  
+                  
+                }else{
+                    
+                    if((word.charAt(i) == 'a' || word.charAt(i) == 'e' || word.charAt(i) == 'i' || word.charAt(i) == 'o' || word.charAt(i) == 'u') && foundvowel == true){
+                        translate = translate + word.charAt(i);
+                    }else{
+                        translate = translate + word.charAt(i);
+                        foundvowel = false;
+                    }
+                }
+            
+            
     }
-    }
+            
+            System.out.print(translate);
+            x = x +1;
+            
+        }
+        
+}
 }
